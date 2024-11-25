bd0f759e9f37fa5d7d5012ca43633248d2ba2e79
HG BOTS <92435453+hemanth233739@users.noreply.github.com>
2024-11-25 18:35:00 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
 VAR_SESSION = None
-VAR_API_ID = "10187126"
-VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
-VAR_MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
+VAR_API_ID = "8577898"
+VAR_API_HASH = "8663682496c76b1240e4b7e527577dad"
+VAR_MONGO_DB_URI = "mongostat --uri mongodb+srv://animalsfunny522:8dtsGr9wcuH9jEIo@cluster0.nyrbh.mongodb.net/?retryWrites=true&w=majority"
 VAR_REPO_URL = "https://github.com/hemanth233739/SophiaUB" # Change to your fork repo
